a213057621c1c6d962c4d3de0e0da558ec78a934
Pieter Beukelman <pieter@beukelman.eu>
2017-03-27 11:38:06 +0200

    Transaction Class implement

--- src/Classes/Transaction.java
+++ src/Classes/Transaction.java
@@ -5,8 +5,26 @@ import java.util.*;
 
 public class Transaction {
 
-	private double amount;
-	Collection<Bid> fromOwnerOf;
-	Collection<Auction> toOwnerOf;
+    private double amount;
+    Collection<Bid> fromOwnerOf;
+    Collection<Auction> toOwnerOf;
+
+    /**
+     * returns the total price
+     *
+     * @return double
+     */
+    public double getAmount() {
+        return 0;
+    }
+
+    /**
+     * returns Auction of transaction
+     *
+     * @return Auction
+     */
+    public Auction getAuction() {
+        return null;
+    }
 
 }
