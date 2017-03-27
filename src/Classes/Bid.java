032da6c096cd223840683aea0460376b2804c6c5
Raaij,Kyle K.S.M.L van <k.vanraaij@student.fontys.nl>
2017-03-27 11:40:34 +0200

    Bid added

--- src/Classes/Bid.java
+++ src/Classes/Bid.java
@@ -1,7 +1,29 @@
 package Classes;
 
+import Classes.Auctions.Auction;
+import java.util.Date;
+
 public class Bid {
 
-	private double amount;
+    private double amount;
+    private Date timeCreated;
+    private User placer;
+
+    public Bid(Auction auction, User placer, double amount) {
+        this.placer = placer;
+        this.amount = amount;
+
+        Date date = new Date();
+        this.timeCreated = date;
+    }
+
+    /**
+     * This metod is used to get a string of the placer of this bid.
+     *
+     * @return User
+     */
+    public String getPlacerUsername() {
+        return this.placer.toString();
+    }
 
 }
