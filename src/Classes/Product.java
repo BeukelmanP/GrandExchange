d81a18be9de1410316ff02b0391a34b0726f8c3a
Pieter Beukelman <pieter@beukelman.eu>
2017-03-27 11:34:12 +0200

    Product Class implement

--- src/Classes/Product.java
+++ src/Classes/Product.java
@@ -2,8 +2,14 @@ package Classes;
 
 public class Product {
 
-	private int GTIN;
-	private String naam;
-	private String beschrijving;
+    private int GTIN;
+    private String naam;
+    private String beschrijving;
 
+    
+    public Product(int GTIN, String naam, String beschrijving) {
+        this.GTIN = GTIN;
+        this.naam = naam;
+        this.beschrijving = beschrijving;
+    }
 }
