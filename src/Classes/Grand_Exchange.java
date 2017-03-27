7a648a8cddf591f9b2985c35b97e657e63f32b39
Pieter Beukelman <pieter@beukelman.eu>
2017-03-27 11:18:20 +0200

    Grand Exchange Class implement

--- src/Classes/Grand_Exchange.java
+++ src/Classes/Grand_Exchange.java
@@ -5,7 +5,79 @@ import java.util.*;
 
 public class Grand_Exchange {
 
-	Collection<Product> products;
-	Collection<User> users;
+    Collection<Product> products;
+    Collection<User> users;
+
+    /**
+     * adds user tot he collection of users
+     *
+     * @param user : may not be null
+     */
+    public void addUser(User user) {
+
+    }
+
+    /**
+     * removes user from collection of users
+     *
+     * @param user : may not be null
+     */
+    public void removeUser(User user) {
+
+    }
+
+    /**
+     * Adds product to collection of products
+     *
+     * @param product : may not be null
+     */
+    public void addProduct(Product product) {
+
+    }
+
+    /**
+     * removes product from collection of products
+     *
+     * @param product : may not be null
+     */
+    public void removeProduct(Product product) {
+
+    }
+
+    /**
+     * Checks user login
+     *
+     * @param username : may not be empty nor null
+     * @param password : may not be empty nor null
+     */
+    public void login(String username, String password) {
+
+    }
+
+    /**
+     * logout of user who is logged in
+     *
+     */
+    public void logout() {
+
+    }
+
+    /**
+     * handles transaction
+     *
+     * @param transaction : may not be null
+     */
+    public void handleTransaction(Transaction transaction) {
+
+    }
+
+    /**
+     * Returns list of all 'official' products
+     *
+     * @return List<Product>
+     */
+    public List<Product> getProducts() {
+        return null;
+    }
 
 }
