1b975db31786c07e69d7a094adf96992dc1bcd39
Raaij,Kyle K.S.M.L van <k.vanraaij@student.fontys.nl>
2017-03-27 13:06:26 +0200

    Unit test Auction

---
+++ test/Classes/Auctions/AuctionTest.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Classes.Auctions;
+
+import Classes.Bid;
+import java.util.List;
+import junit.framework.Assert;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author kyle_
+ */
+public class AuctionTest {
+
+    Auction auction;
+
+    public AuctionTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getBestBid method, of class Auction. This test checks if result
+     * is not null.
+     */
+    @Test
+    public void testGetBestBid() {
+        Bid result = auction.getBestBid();
+        Assert.assertNotNull("Bid is null", result);
+    }
+
+    /**
+     * Test of getBids method, of class Auction. This test checks if the list is
+     * not empty
+     */
+    @Test
+    public void testGetBids() {
+        List<Bid> bids = auction.getBids();
+        if (bids.isEmpty()) {
+            fail("The list is empty");
+        }
+    }
+
+    /**
+     * Test of sendMailRequest method, of class Auction.
+     */
+    @Test
+    public void testSendMailRequest() {
+        String email = "Kyle_v_r@hotmail.com";
+
+        try {
+            auction.sendMailRequest(email);
+        } catch (Exception e) {
+            fail("Method failed");
+        }
+
+    }
+
+}
