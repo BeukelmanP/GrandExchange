99c7c39bf6d7c84e58e3914fa6fed516cc8a3c22
Lesley student <lesley.peters@live.nl>
2017-03-27 13:24:12 +0200

    Added getters for product and added unit tests.

--- src/Classes/Product.java
+++ src/Classes/Product.java
@@ -2,14 +2,26 @@ package Classes;
 
 public class Product {
 
-    private int GTIN;
+    private String GTIN;
     private String name;
     private String description;
 
     
-    public Product(int GTIN, String name, String description) {
+    public Product(String GTIN, String name, String description) {
         this.GTIN = GTIN;
         this.name = name;
         this.description = description;
+    }
+    
+    public String getGTIN(){
+        return this.GTIN;
+    }
+    
+    public String getName(){
+        return this.name;
+    }
+    
+    public String getDescription(){
+        return this.description;
     }
 }
