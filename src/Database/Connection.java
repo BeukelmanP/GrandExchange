6e4e24a884d63c14c2a9e32d94581b03df87dcb1
I334389 <j.vaes@student.fontys.nl>
2017-04-03 10:21:59 +0100

    Merge origin/master
    Conflicts:
    	Risk-IT/test/Classes/AccountTest.java
    	Risk-IT/test/Classes/AreaTest.java
    	Risk-IT/test/Classes/AttackTest.java
    	Risk-IT/test/Classes/ContinentTest.java
    	Risk-IT/test/Classes/GameResultTest.java
    	Risk-IT/test/Classes/LobbyTest.java
    	Risk-IT/test/Classes/LogTest.java
    	Risk-IT/test/Classes/PlayerTest.java
    	Risk-IT/test/Classes/ReinforcementTest.java

--- src/Database/Connection.java
+++ src/Database/Connection.java
@@ -5,18 +5,87 @@
  */
 package Database;
 
+import Classes.Auctions.Auction;
+import java.io.IOException;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
- * @author piete
+ * @author Jorian
  */
 public class Connection {
+    
+    private java.sql.Connection myConn = null;
+    private PreparedStatement pstmt = null;
+    private Statement myStmt = null;
+    private ResultSet myRs = null;
+    ArrayList<Auction> auctions;
+    
+    static final String GET_FROM_AUCTIONS_SQL = "SELECT ? FROM ? WHERE ID = ?";
+
+    
+    
     public Connection()
     {
     
     }
     
-    public void getConnection()
-    {
-    
+    public void getConnection(){
+        try {
+            myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/dbName", "Login" , "Pass");
+        } 
+        catch (SQLException ex) {
+            Logger.getLogger(Connection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        System.out.println("Starting connection to databse...");
     }
+    
+    public Collection<Auction> getAuctions(String select, String from, String where, String is){
+        
+        auctions = new ArrayList<Auction>() {};
+    
+        try {
+            getConnection();
+            pstmt = myConn.prepareStatement(GET_FROM_AUCTIONS_SQL);
+            pstmt.setString(1, select);
+            pstmt.setString(1, from);
+            pstmt.setString(1, where);
+            pstmt.setString(1, is);
+            
+            myRs = pstmt.executeQuery();
+            myRs.next();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+            Logger.getLogger(Connection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
+        try {
+            myRs.getRow();
+        } catch (Exception e) {
+        }
+        
+        return auctions;
+    }
+    
+    
+    private void closeConnection(){
+        try {
+            myRs.close();
+            myConn.close();
+            pstmt.close();
+            System.out.println("Closing connection to database...");
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+            Logger.getLogger(Connection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    };
 }
